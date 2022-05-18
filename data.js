2cac1b58b7fc6cfb96a2c5f54a498ff2d0767b73
Munkhjaw <78463890+Munkhjaw@users.noreply.github.com>
2022-05-18 10:21:37 +0800

    ok
    ok

---
+++ data.js
@@ -0,0 +1,105 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bayankhongor",
+      "name": "Bayankhongor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3519.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6298695413102049,
+          "pitch": 0.9411589650915886,
+          "rotation": 0,
+          "target": "1-altay-tawanbogd"
+        },
+        {
+          "yaw": 0.5374854873754007,
+          "pitch": 1.0388096934789601,
+          "rotation": 0,
+          "target": "2-ulaagchyn-black-lake"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-altay-tawanbogd",
+      "name": "Altay-TawanBogd",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ulaagchyn-black-lake",
+      "name": "Ulaagchyn Black Lake",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "BSA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
